1827ff700d7769d4593e2cf13394eaebd1ef45e2
ARUN AK <arunarjunan9083@gmail.com>
2026-06-06 13:27:09 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,10 @@
+integer_type,float_type,bool_type,string_type,list_type=12,1.2,False,"arun",[1,2,3,4,5]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
